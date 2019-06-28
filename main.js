990b4873c69a3ad563a8e952263ddb3d9ab34adc
Nicolas Ferreyra <nferreyra@v-n.com.ar>
2019-06-28 15:55:31 -0300

    formulario css

--- main.js
+++ main.js
@@ -1,21 +1,13 @@
-let caja = document.getElementsByTagName('div');
 
-caja[0].textContent = 'Q mirai';
 
-let ca = document.createElement('div');
-ca.textContent= 'Holus';
-ca.setAttribute('class','caja naranja');
-ca.setAttribute('id','id1');
+let ca = document.getElementsByClassName('caja');
 
-let contenedor = document.getElementById('contenedor');
+function tog(){
+ this.classList.toggle('naranja');
+}
 
-contenedor.appendChild(ca);
-
-let boton = document.getElementById('fua');
-
-boton.addEventListener('mouseover', ()=>{
-    var ca= document.getElementById('id1');
-    ca.setAttribute('class','caja');
-});
+for(let i = 0 ; i<= ca.length ; i++){
+    ca[i].addEventListener('click',tog)
+}
 
 
